014708e092d0d32b75aa92910d0cbf906dd80ab2
tennis348 <vinaysb2000@gmail.com>
2018-01-06 06:39:35 +0530

    Created for learning purposes!

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+
+    $(".js-way-1").waypoint(function () {
+        $(".js-way-1").addClass('animated fadeInUp');
+    }, {
+        offset: "60%"
+    });
+
+    $(".js-way-2").waypoint(function () {
+        $(".js-way-2").addClass('animated pulse');
+    }, {
+        offset: "40%"
+    });
+
+    $(".js-way-3").waypoint(function () {
+        $(".js-way-3").addClass('animated fadeInLeft');
+    }, {
+        offset: "100%"
+    });
+
+    $(".js-way-4").waypoint(function () {
+        $(".js-way-4").addClass('animated fadeInRight');
+    }, {
+        offset: "100%"
+    });
+
+    $(".js-way-5").waypoint(function () {
+        $(".js-way-5").addClass('animated fadeInLeft');
+    }, {
+        offset: "100%"
+    });
+
+    $(".js-way-6").waypoint(function () {
+        $(".js-way-6").addClass('animated fadeInRight');
+    }, {
+        offset: "100%"
+    });
+
+    $(".js-way-7").waypoint(function () {
+        $(".js-way-7").addClass('animated fadeInLeft');
+    }, {
+        offset: "100%"
+    });
+
+    $(".js-way-8").waypoint(function () {
+        $(".js-way-8").addClass('animated fadeInRight');
+    }, {
+        offset: "100%"
+    });
+
+})
